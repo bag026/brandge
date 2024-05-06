9979d718f88c6cc3c2815216aa79656549f9e562
bag026 <gebaojun@yahoo.com>
2024-05-06 12:16:57 +0800

    this is testing

--- scripts_bio.js
+++ scripts_bio.js
@@ -1,10 +1,5 @@
 //Navigation structure
 document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll('.navbar a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.forEach(lnk => lnk.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
+
+
 });
